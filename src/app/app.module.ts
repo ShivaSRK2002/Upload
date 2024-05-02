19fca51288e7df8d4f1b74c7349ffd0db9a082f3
ShivaSRK2002 <shivaramakrshnn@gmail.com>
2024-05-02 15:00:20 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FileUploadComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
